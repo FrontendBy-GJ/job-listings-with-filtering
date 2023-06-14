e0cc85637d695ba0dd7bbca961e189cc7a6ebd5b
Genaro Garcia <genaro.dev@outlook.com>
2023-06-14 13:39:27 -0500

    feat: add responsive header background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import mobileBG from './assets/bg-header-mobile.svg';
+import desktopBG from './assets/bg-header-desktop.svg';
+
 function App() {
-  return <></>;
+  return (
+    <div className="mx-auto min-h-screen max-w-[1440px]">
+      <div className="bg-Primary-Dark-Cyan">
+        <picture>
+          <source media="(min-width: 640px)" srcSet={desktopBG} />
+          <img src={mobileBG} alt="" className="w-full" />
+        </picture>
+      </div>
+    </div>
+  );
 }
 
 export default App;
