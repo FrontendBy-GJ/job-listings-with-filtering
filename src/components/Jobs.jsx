35b199b98d6c92c5d4810c8f6adaac2f13ae8cb9
Genaro Garcia <genaro.dev@outlook.com>
2023-06-15 12:00:43 -0500

    feat: add Jobs component to display job listings

---
+++ src/components/Jobs.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Job from '../components/Job';
+import useJobContext from '../hooks/useJobContext';
+
+const Jobs = () => {
+  const { data, filters } = useJobContext();
+
+  const catergoryMatchesFilter = (cat) => {
+    // destructure role, level, languages, tools from cat
+    const { role, level, languages, tools } = cat;
+
+    // array containing all values from properties
+    // spread (...) includes all elements from languages and tools
+    const allCategories = [role, level, ...languages, ...tools];
+
+    // every method checks if every element in the filters array is included in the allCategories array
+    return filters.every((selectedFilter) =>
+      allCategories.includes(selectedFilter)
+    );
+  };
+
+  const filteredCategories = data.filter((filter) =>
+    filter.length === 0 ? true : catergoryMatchesFilter(filter)
+  );
+
+  return (
+    <div className="mx-auto grid grid-cols-1 gap-12 px-5 pb-10 pt-16 md:max-w-2xl md:gap-5 md:pb-24 lg:max-w-4xl xl:max-w-6xl">
+      {filteredCategories.map((cat) => (
+        <Job key={cat.id} data={cat} />
+      ))}
+    </div>
+  );
+};
+
+export default Jobs;
