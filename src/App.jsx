4e1f749d84d88af59ef0760bf2fce2bf00051462
Genaro Garcia <genaro.dev@outlook.com>
2023-06-15 12:03:50 -0500

    feat: add JobContextProvider and Jobs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import mobileBG from './assets/bg-header-mobile.svg';
 import desktopBG from './assets/bg-header-desktop.svg';
+import Jobs from './components/Jobs';
+import JobContextProvider from './context/JobContext';
 
 function App() {
   return (
-    <div className="mx-auto min-h-screen max-w-[1440px]">
+    <div className="mx-auto min-h-screen max-w-[1440px] bg-Neutral-Light-Cyan-BG">
       <div className="bg-Primary-Dark-Cyan">
         <picture>
           <source media="(min-width: 640px)" srcSet={desktopBG} />
           <img src={mobileBG} alt="" className="w-full" />
         </picture>
       </div>
+      <JobContextProvider>
+        <Jobs />
+      </JobContextProvider>
     </div>
   );
 }
